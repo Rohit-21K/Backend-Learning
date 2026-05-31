d371ee60f45fbae1fb879aeaef9c1e401d96f15e
Rohitkash21 <rohitkash21@gmail.com>
2026-05-31 08:50:28 +0530

    utils files created

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+// cors settings
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+export { app };
